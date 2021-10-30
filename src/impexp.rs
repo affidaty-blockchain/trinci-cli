61b7a8d9d7843310dd7ceff0e3225e6e954de56a
Davide Galassi <davxy@datawok.net>
2021-10-30 19:57:32 +0200

    Better export file header
    Added magic number

--- src/impexp.rs
+++ src/impexp.rs
@@ -1,11 +1,19 @@
 use crate::client::Client;
 use indicatif::{ProgressBar, ProgressStyle};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use std::{fs::File, io::Read, io::Write};
 use trinci_core::{
     base::serialize::{rmp_deserialize, rmp_serialize},
     Transaction,
 };
+
+const MAGIC: u64 = 0xe7aa30ddc4db5bdd;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FileHeader {
+    magic: u64,
+    num_blocks: u64,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FileBlock {
@@ -28,23 +36,21 @@ fn read_len(file: &mut File) -> Option<u64> {
     }
 }
 
-impl FileBlock {
-    fn from_file(file: &mut File) -> Option<Self> {
-        let len = match read_len(file) {
-            Some(len) => len,
-            None => return None,
-        };
-        let mut buf: Vec<u8> = vec![0; len as usize];
-        file.read_exact(&mut buf).expect("Unexpected file content");
-        let blk: FileBlock = rmp_deserialize(&buf).expect("Deserializing block");
-        Some(blk)
-    }
+fn write_to_file<T: Serialize>(file: &mut File, value: T) {
+    let buf = rmp_serialize(&value).expect("serializing");
+    write_len(file, buf.len() as u64);
+    file.write_all(&buf).expect("Writing to file");
+}
 
-    fn to_file(&self, file: &mut File) {
-        let buf = rmp_serialize(&self).expect("Serializing block");
-        write_len(file, buf.len() as u64);
-        file.write_all(&buf).expect("Writing to file");
-    }
+fn read_from_file<T: DeserializeOwned>(file: &mut File) -> Option<T> {
+    let len = match read_len(file) {
+        Some(len) => len,
+        None => return None,
+    };
+    let mut buf: Vec<u8> = vec![0; len as usize];
+    file.read_exact(&mut buf).expect("Unexpected file content");
+    let value: T = rmp_deserialize(&buf).expect("Deserializing block");
+    Some(value)
 }
 
 pub fn export_txs(file_name: &str, client: &mut Client) {
@@ -61,8 +67,11 @@ pub fn export_txs(file_name: &str, client: &mut Client) {
             .progress_chars("#>-"),
     );
 
-    // write total number of blocks
-    write_len(&mut file, last_block.height + 1);
+    let head = FileHeader {
+        magic: MAGIC,
+        num_blocks: last_block.height + 1,
+    };
+    write_to_file(&mut file, head);
 
     for i in 0..=last_block.height {
         let (_block, tx_hashes) = client.get_block(i).unwrap();
@@ -75,7 +84,7 @@ pub fn export_txs(file_name: &str, client: &mut Client) {
             txs.push(tx);
         });
         let file_blk = FileBlock { height: i, txs };
-        file_blk.to_file(&mut file);
+        write_to_file(&mut file, file_blk);
 
         bar.set_message(format!("blocks: {}/{}", i + 1, last_block.height + 1));
         bar.inc(1);
@@ -91,17 +100,20 @@ pub fn import_txs(file_name: &str, client: &mut Client) {
         Ok(file) => file,
     };
 
-    let num_blocks = read_len(&mut file).expect("Bad file format");
+    let head: FileHeader = read_from_file(&mut file).expect("Bad file format");
+    if head.magic != MAGIC {
+        panic!("Bad magic number not found");
+    }
 
-    let bar = ProgressBar::new(num_blocks);
+    let bar = ProgressBar::new(head.num_blocks);
     bar.set_style(
         ProgressStyle::default_bar()
             .template("[{elapsed_precise}] {bar:40.cyan/blue}  {msg}")
             .progress_chars("#>-"),
     );
 
-    for i in 0..num_blocks {
-        let file_blk = match FileBlock::from_file(&mut file) {
+    for i in 0..head.num_blocks {
+        let file_blk = match read_from_file::<FileBlock>(&mut file) {
             Some(blk) => blk,
             None => panic!("Unexpected file early termination"),
         };
@@ -130,17 +142,17 @@ pub fn import_txs(file_name: &str, client: &mut Client) {
             }
         }
 
-        bar.set_message(format!("blocks: {}/{}", i + 1, num_blocks));
+        bar.set_message(format!("blocks: {}/{}", i + 1, head.num_blocks));
         bar.inc(1);
     }
     bar.finish();
 
     let (last_block, _) = client.get_block(u64::MAX).expect("No block");
-    if last_block.height + 1 != num_blocks {
+    if last_block.height + 1 != head.num_blocks {
         println!(
             "Warning: number of blocks differ from the original one (produced: {}, expected {})",
             last_block.height + 1,
-            num_blocks
+            head.num_blocks
         );
     }
     println!("All transaction imported from '{}'", file_name);
